2693cb10131b2bdd2ace88376dcb979037903e04
Green Learner <greenlearner01@users.noreply.github.com>
2019-07-23 09:36:12 +0530

    Update UserManagementAppApplication.java

--- userManagementApp/src/main/java/com/gl/userManagementApp/UserManagementAppApplication.java
+++ userManagementApp/src/main/java/com/gl/userManagementApp/UserManagementAppApplication.java
@@ -3,7 +3,7 @@ package com.gl.userManagementApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class UserManagementAppApplication {
 
 	public static void main(String[] args) {
